d2a309ce2373f31c21c980b6ba6e08d598e9721a
zhongxiaohong <931766850@qq.com>
2020-07-08 18:06:39 +0800

    6241201

---
+++ app/src/main/java/com/silang/highgradeui/TestGoogleFlexboxLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.silang.highgradeui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestGoogleFlexboxLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_google_flexbox_layout)
+    }
+
+}
