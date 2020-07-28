32f164572637a502c91843ad5450a4bc192a7836
zhongxiaohong <931766850@qq.com>
2020-07-28 21:53:41 +0800

    fdf

---
+++ app/src/main/java/viewpager/ViewPagerActivity.java
@@ -0,0 +1,16 @@
+package viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.silang.highgradeui.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+    }
+}
