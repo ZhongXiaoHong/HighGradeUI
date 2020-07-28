1894b1c751d37a729f4bd8a1383595166eca3e95
zhongxiaohong <931766850@qq.com>
2020-07-29 07:43:02 +0800

    fdf

--- app/src/main/java/viewpager/ViewPagerActivity.java
+++ app/src/main/java/viewpager/ViewPagerActivity.java
@@ -1,16 +1,47 @@
 package viewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.silang.highgradeui.R;
 
 public class ViewPagerActivity extends AppCompatActivity {
 
+    ViewPager vp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+        
+        vp = findViewById(R.id.vp);
+        vp.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+                return view==object;
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+               container.addView();
+            }
+
+            @Override
+            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+                container.removeView((View)object);
+            }
+        });
     }
 }
